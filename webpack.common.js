32c15ebbee8bfd1e03cc9b18846bbddd3f77754a
Burhan <burhan221b@gmail.com>
2021-01-02 00:34:41 -0800

    First Commit -m Basic Setup

---
+++ webpack.common.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    // devtool: 'inline-soruce-map',
+    entry: "./src/index.tsx",
+    module: {
+        rules: [
+            { test: /\.tsx?$/, use: "ts-loader", include: path.resolve(__dirname, "src"), exclude: /node_modules/ },
+            { test: /.html$/, use: "html-loader" },
+            { test: /\.(svg|png|jpe?g|gif)$/, use: [{ loader: 'file-loader', options: { name: "[name].[hash].[ext]", outputPath: "assets" } }] },
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
+    watch: true,
+    resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'] },
+}
---
+++ webpack.dev.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const common = require('./webpack.common');
+const { merge } = require('webpack-merge');
+
+module.exports = merge(
+    common,
+    {
+        mode: 'development',
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'public'),
+            publicPath: '/'
+        },
+        module: {
+            rules: [{
+                test: /\.scss$/, use: [
+                    "style-loader", // 3. Inject style tags into DOM
+                    "css-loader", // 2. Turns css into commonjs
+                    "sass-loader" // 1. Turns sass into css
+                ], include: path.resolve(__dirname, 'src'), exclude: /node_modules/
+            }]
+        },
+        devServer: {
+            // Reference for mobile: https://stackoverflow.com/questions/35412137/how-to-get-access-to-webpack-dev-server-from-devices-in-local-network
+            host: '0.0.0.0',//your ip address
+            port: 8080,
+            historyApiFallback: true,
+        },
+    }
+)
