a9c21aef747c1e30e6c64bce6afc5adb4ce8d398
Burhan <burhan221b@gmail.com>
2021-01-02 15:11:15 -0800

    minor gui change

--- src/components/NavTop.tsx
+++ src/components/NavTop.tsx
@@ -10,7 +10,7 @@ const NavTop: React.FunctionComponent<NavTopProps> = () => {
     return (
         <nav className="nav" id="nav-top">
             <button className="nav-top-left-menu-button">&#9776;</button>
-            <h2 className="nav-top-main-title">PlaceHolderForSomething</h2>
+            <h3 className="nav-top-main-title">PlaceHolder</h3>
         </nav>
     );
 }
