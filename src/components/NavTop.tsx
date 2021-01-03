b6e0c863b5554dcb0d51449f1688824f561b3e4c
Burhan <burhan221b@gmail.com>
2021-01-02 16:55:18 -0800

    added comment about top navs

--- src/components/NavTop.tsx
+++ src/components/NavTop.tsx
@@ -9,6 +9,10 @@ const NavTop: React.FunctionComponent<NavTopProps> = () => {
     hideMenuOnScrollDown()
     return (
         <nav className="nav" id="nav-top">
+            {/* 
+            The button will be either menu button (3 bars) or 
+            left arrow back button 
+            */}
             <button className="nav-top-left-menu-button">&#9776;</button>
             <h3 className="nav-top-main-title">PlaceHolder</h3>
         </nav>
